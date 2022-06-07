9d0a1ee7ed669875020da3783c1df9c9ff429a2d
CuongFlodric <lekubin00@gmail.com>
2022-06-07 11:18:23 +0700

    First commit

---
+++ Demo-Detox/android/app/build/generated/source/buildConfig/androidTest/debug/com/jsmarathon/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.jsmarathon.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.jsmarathon.test";
+  public static final String BUILD_TYPE = "debug";
+}
